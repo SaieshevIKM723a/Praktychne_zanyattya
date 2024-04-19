e6e50a39344fa0d607bde47c03de9e6a80d46a18
SaieshevIKM723a <162920666+SaieshevIKM723a@users.noreply.github.com>
2024-04-19 22:50:02 +0300

    Add files via upload

---
+++ Завдання 2/Завдання 2/Завдання 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter number: ";
+    cin >> num;
+
+    int result = factorial(num);
+
+    cout << "Factorial: " << num << " Sum: " << result << endl;
+
+    return 0;
+}
