339ef1a4729d71fa031ea8e5c47069a761d71ed1
Damir <Damir@DESKTOP-ELD43TT>
2024-04-19 22:54:00 +0300

    Добавьте файлы проекта.

---
+++ Завдання 6/Завдання 6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseString(string str) {
+    int length = str.length();
+    for (int i = 0; i < length / 2; i++) {
+        swap(str[i], str[length - i - 1]);
+    }
+    return str;
+}
+
+int main() {
+    string orig = "Hello, world!";
+    string reversedstr = reverseString(orig);
+
+    cout << "Original row: " << orig << endl;
+    cout << "Reversed row: " << reversedstr << endl;
+
+    return 0;
+}
