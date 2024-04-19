2e780f9d182ae8fb45f739b895b5851ae433c7d5
SaieshevIKM723a <162920666+SaieshevIKM723a@users.noreply.github.com>
2024-04-19 22:49:09 +0300

    Add files via upload

---
+++ Завдання 5/Завдання 5/Завдання 5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int sumOfArray(int array[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int main() {
+    int array[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(array) / sizeof(array[0]);
+
+    int totalsum = sumOfArray(array, size);
+
+    cout << "Summ of elements of array: " << totalsum << endl;
+
+    return 0;
+}
