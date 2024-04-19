960cb132552e7b75011790a037048bf4a813a1c2
SaieshevIKM723a <162920666+SaieshevIKM723a@users.noreply.github.com>
2024-04-19 22:49:49 +0300

    Add files via upload

---
+++ Завдання 3/ConsoleApplication26/ConsoleApplication26.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int getLength(string str) {
+    int length = 0;
+    for (char c : str) {
+        length++;
+    }
+    return length;
+}
+
+int main() {
+    string str = "Hello KHPI!";
+    int length = getLength(str);
+    cout << "Length of row" << str << "' : " << length << endl;
+    return 0;
+}
